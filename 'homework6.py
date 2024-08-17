00e81cc335e4e8999aa08c57ea2902d3810025fd
evgenia kolmakova <evgeniageller000@gmail.com>
2024-08-17 13:26:21 +0300

    Add files via upload

--- 'homework6.py
+++ 'homework6.py
@@ -1,19 +1,19 @@
-#my_dict = {'Kate': 1999, 'Pasha': 1998}
-#print(my_dict['Kate'])
-#print(my_dict.get ('Max'))
-#my_dict ['Anton']= 1989
-#print(my_dict['Anton'])
-#my_dict['Lola']=1995
-#f = my_dict.pop('Anton')
-#print(f)
-#print(my_dict)
-#print(my_dict.keys())
+my_dict = {'Kate': 1999, 'Pasha': 1998}
+print(my_dict['Kate'])
+print(my_dict.get ('Max'))
+my_dict ['Anton']= 1989
+print(my_dict['Anton'])
+my_dict['Lola']=1995
+f = my_dict.pop('Anton')
+print(f)
+print(my_dict)
+print(my_dict.keys())
 
 my_set = {1, 2, 2, 5, 6, "King","King", True, (10, 11, 12, 13)}
 print(my_set)
-print(my_set.add(7))
+(my_set.add(7))
 print(my_set)
-print(my_set.add(9))
+(my_set.add(9))
 print(my_set)
 my_set.discard(1)
 print(my_set)
